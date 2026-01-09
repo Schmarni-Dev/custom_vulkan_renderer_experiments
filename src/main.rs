49c09a0e3a73d74edca3e869648cd89e7285c74b
Schmarni <marnistromer@gmail.com>
2026-01-09 12:21:03 +0100

    impl test code for timeline semaphore importing
    Signed-off-by: Schmarni <marnistromer@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,7 @@
-use std::{
-    sync::Arc,
-    time::{Duration, Instant},
-};
+use std::{sync::Arc, time::{Duration, Instant}};
 
-use glam::{Mat4, Quat, UVec2, Vec3, vec3};
-use stardust_xr_fusion::{
-    AsyncEventHandle, Client, ClientHandle,
-    drawable::{MaterialParameter, Model, ModelPartAspect, get_primary_render_device_id},
-    project_local_resources,
-    root::{RootAspect, RootEvent},
-    spatial::Transform,
-    values::ResourceID,
-};
-use timeline_syncobj::render_node::DrmRenderNode;
-use tokio::time::sleep;
+use glam::{Mat4, UVec2, vec3};
+use timeline_syncobj::{render_node::DrmRenderNode, timeline_syncobj::TimelineSyncObj};
 use tracing::{info, warn};
 use vulkano::{
     VulkanLibrary,
@@ -26,25 +14,21 @@ use vulkano::{
         physical::{PhysicalDevice, PhysicalDeviceType},
     },
     format::Format,
-    image::{ImageUsage, view::ImageView},
-    instance::{Instance, InstanceCreateFlags, InstanceCreateInfo, InstanceExtensions},
-    memory::allocator::StandardMemoryAllocator,
+    image::{Image, ImageUsage, view::ImageView},
+    instance::{Instance, InstanceCreateInfo, InstanceExtensions},
+    memory::allocator::{AllocationCreateInfo, StandardMemoryAllocator},
     sync::{
         PipelineStages,
-        semaphore::{Semaphore, SemaphoreWaitFlags, SemaphoreWaitInfo},
+        semaphore::{ExternalSemaphoreHandleType, ImportSemaphoreFdInfo, Semaphore, SemaphoreType},
     },
 };
 
-use crate::{
-    renderer::{RenderPipeline, Renderer, View},
-    stardust_backend::{DmatexFormat, Swapchain, get_phys_dev_node_id},
-    winit_backend::WinitBackend,
-};
+use crate::renderer::{RenderPipeline, Renderer, View};
 
 pub mod mesh;
 pub mod renderer;
-pub mod stardust_backend;
-pub mod winit_backend;
+// pub mod stardust_backend;
+// pub mod winit_backend;
 
 pub trait Backend {
     type Init;
@@ -60,22 +44,12 @@ pub trait Backend {
     fn create(init: Self::Init, renderer: Arc<Renderer>) -> Self;
 }
 
-#[tokio::main]
-async fn main() {
+fn main() {
     tracing_subscriber::fmt().init();
     info!("Hello, world!");
-    let client = Client::connect().await.unwrap();
-    client
-        .setup_resources(&[&project_local_resources!("res")])
-        .unwrap();
-    let async_loop = client.async_event_loop();
-    let client = async_loop.client_handle.clone();
-    let render_node_id = get_primary_render_device_id(&client).await.unwrap();
 
-    let winit_init = WinitBackend::create_init();
     let library = VulkanLibrary::new().unwrap();
-    let required_extensions =
-        WinitBackend::required_instance_exts(&winit_init) | Renderer::required_instance_exts();
+    let required_extensions = Renderer::required_instance_exts();
     if library.api_version() < Renderer::required_vulkan_version() {
         panic!("vulkan 1.2 not supported")
     }
@@ -89,7 +63,6 @@ async fn main() {
     .unwrap();
 
     let device_extensions = DeviceExtensions {
-        khr_swapchain: true,
         // TODO: move this to stardust specific code
         ext_image_drm_format_modifier: true,
         ext_external_memory_dma_buf: true,
@@ -103,15 +76,11 @@ async fn main() {
         .enumerate_physical_devices()
         .unwrap()
         .filter(|p| p.supported_extensions().contains(&device_extensions))
-        .filter(|p| get_phys_dev_node_id(p) == render_node_id)
         .filter_map(|p| {
             p.queue_family_properties()
                 .iter()
                 .enumerate()
-                .position(|(i, q)| {
-                    q.queue_flags.intersects(QueueFlags::GRAPHICS)
-                        && WinitBackend::supports_queue_type(&winit_init, &p, i as u32)
-                })
+                .position(|(_i, q)| q.queue_flags.intersects(QueueFlags::GRAPHICS))
                 .map(|i| (p, i as u32))
         })
         .min_by_key(|(p, _)| match p.properties().device_type {
@@ -159,65 +128,52 @@ async fn main() {
         queue,
         command_buffer_allocator,
     ));
-    tokio::spawn(stardust_loop(
-        async_loop.get_event_handle(),
-        client,
-        renderer.clone(),
-    ))
-    .await
-    .unwrap();
-
-    // tokio::task::block_in_place(|| {
-    //     WinitBackend::create(winit_init, renderer.clone()).run();
-    // });
+    stardust_loop(renderer);
 }
 
-async fn stardust_loop(
-    event_handle: AsyncEventHandle,
-    client: Arc<ClientHandle>,
-    renderer: Arc<Renderer>,
-) {
+fn stardust_loop(renderer: Arc<Renderer>) {
     let res = UVec2::new(512, 512);
-    let render_node_id = get_primary_render_device_id(&client).await.unwrap();
+    let render_node_id = 128;
     let render_node = DrmRenderNode::new(render_node_id).unwrap();
-    let model = Model::create(
-        client.get_root(),
-        Transform::identity(),
-        &ResourceID::new_namespaced("vk", "panel"),
+    info!("drm node");
+    let render_pipeline = RenderPipeline::new(&renderer, Format::R8G8B8A8_SRGB, None);
+    info!("pipeline");
+    let image = Image::new(
+        renderer.malloc.clone(),
+        vulkano::image::ImageCreateInfo {
+            format: Format::R8G8B8A8_SRGB,
+            extent: [res.x, res.y, 1],
+            usage: ImageUsage::COLOR_ATTACHMENT,
+            ..Default::default()
+        },
+        AllocationCreateInfo::default(),
     )
     .unwrap();
-    let formats = DmatexFormat::enumerate(&client, render_node_id)
-        .await
-        .unwrap();
-    let format = formats.get(&Format::R8G8B8A8_SRGB).unwrap();
-    let render_pipeline = RenderPipeline::new(&renderer, Format::R8G8B8A8_SRGB, None);
-    let mut swapchain = Swapchain::new(
-        &client,
-        &renderer.dev,
-        &render_node,
-        stardust_xr_fusion::drawable::DmatexSize::Dim2D(res.into()),
-        format,
-        None,
-        ImageUsage::COLOR_ATTACHMENT,
+    info!("image");
+    let syncobj = TimelineSyncObj::create(&render_node).unwrap();
+    info!("syncobj");
+    let semaphore = Arc::new(
+        Semaphore::new(
+            renderer.dev.clone(),
+            vulkano::sync::semaphore::SemaphoreCreateInfo {
+                semaphore_type: SemaphoreType::Timeline,
+                ..Default::default()
+            },
+        )
+        .unwrap(),
     );
-    let panel = model.part("Panel").unwrap();
-    sleep(Duration::from_millis(100)).await;
+    info!("pre semaphore import info");
+    unsafe {
+        semaphore
+            .import_fd(ImportSemaphoreFdInfo {
+                file: Some(syncobj.export().unwrap().into()),
+                ..ImportSemaphoreFdInfo::handle_type(ExternalSemaphoreHandleType::OpaqueFd)
+            })
+            .unwrap()
+    };
+    info!("post semaphore import info");
+    let mut wait_point = 1;
     loop {
-        event_handle.wait().await;
-        let frame_info = match client.get_root().recv_root_event() {
-            Some(RootEvent::Ping { response }) => {
-                response.send_ok(());
-                continue;
-            }
-            Some(RootEvent::Frame { info }) => info,
-            _ => {
-                continue;
-            }
-        };
-        info!("about to wait on next image");
-        let info = swapchain.next().await;
-        info!("waited on next image");
-
         let ratio = res.x as f32 / res.y as f32;
         let mat = Mat4::perspective_rh(90f32.to_radians(), ratio, 0.0, 1000.0)
             // * (Mat4::from_quat(Quat::from_rotation_z(0.3 * frame_info.elapsed))
@@ -236,39 +192,22 @@ async fn stardust_loop(
         .unwrap();
         renderer.record_render_commands(
             &[View { world_to_clip: mat }],
-            ImageView::new_default(info.image).unwrap(),
+            ImageView::new_default(image.clone()).unwrap(),
             vertex_positions,
             &render_pipeline,
             &mut builder,
         );
         let buffer = builder.build().unwrap();
-        let timeline_semaphore = Arc::new(
-            Semaphore::new(
-                renderer.dev.clone(),
-                vulkano::sync::semaphore::SemaphoreCreateInfo {
-                    semaphore_type: vulkano::sync::semaphore::SemaphoreType::Timeline,
-                    initial_value: 0,
-                    ..Default::default()
-                },
-            )
-            .unwrap(),
-        );
         renderer.render_queue.with(|mut queue| unsafe {
             queue
                 .submit(
                     &[SubmitInfo {
                         command_buffers: vec![CommandBufferSubmitInfo::new(buffer)],
-                        signal_semaphores: vec![
-                            // SemaphoreSubmitInfo {
-                            //     stages: PipelineStages::COLOR_ATTACHMENT_OUTPUT,
-                            //     ..SemaphoreSubmitInfo::new(info.server_acquire_semaphore)
-                            // },
-                            SemaphoreSubmitInfo {
-                                value: 1,
-                                stages: PipelineStages::ALL_COMMANDS,
-                                ..SemaphoreSubmitInfo::new(timeline_semaphore.clone())
-                            },
-                        ],
+                        signal_semaphores: vec![SemaphoreSubmitInfo {
+                            value: wait_point,
+                            stages: PipelineStages::ALL_COMMANDS,
+                            ..SemaphoreSubmitInfo::new(semaphore.clone())
+                        }],
                         ..Default::default()
                     }],
                     None,
@@ -276,22 +215,13 @@ async fn stardust_loop(
                 .unwrap()
         });
         let time = Instant::now();
-        timeline_semaphore
-            .wait(
-                SemaphoreWaitInfo {
-                    value: 1,
-                    ..Default::default()
-                },
-                None,
-            )
-            .unwrap();
-        let diff = time.elapsed().as_secs_f64() * 1000.0;
 
-        warn!("render time: {diff}ms");
-        unsafe { info.tex.timeline.signal(info.material_param.acquire_point) }.unwrap();
-        panel
-            .set_material_parameter("diffuse", MaterialParameter::Dmatex(info.material_param))
-            .unwrap();
-        info!("param set");
+        syncobj.blocking_wait(wait_point, None).unwrap();
+        let diff = time.elapsed().as_secs_f64() * 1000.0;
+        let time = Instant::now();
+        let diff2 = time.elapsed().as_secs_f64() * 1000.0;
+
+        println!("render time: {diff}ms, baseline time: {diff2}");
+        wait_point += 1;
     }
 }
